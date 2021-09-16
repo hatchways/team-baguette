9564b5e1800b85816422a1bf47a432780d8325fd
Logan <mlogan858@hotmail.com>
2021-09-15 21:34:49 -0400

    request model created

---
+++ server/models/Request.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+
+const requestSchema = new mongoose.Schema(
+    {
+      user_id: {
+        type: String,
+        required: true,
+      },
+      sitter_id: {
+        type: String,
+      },
+      start: {
+        type: Date,
+        required: true,
+      },
+      end: {
+        type: Date,
+        required: true,
+      },
+      accepted: {
+        type: Boolean,
+        default: false,
+      },
+      declined: {
+        type: Boolean,
+        default: false,
+      },
+      paid: {
+        type: Boolean,
+        default: false,
+      },
+      dogType: {
+        type: String,
+        maxLength: 30,
+      },
+      specialNotes: {
+        type: String,
+        maxLength: 255,
+      },
+    },
+    {
+      timestamps: true,
+    },
+)
+
+module.exports = Request = mongoose.model("request", requestSchema)
