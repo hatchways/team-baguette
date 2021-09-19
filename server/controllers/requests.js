bfe5ccb05487d56dbd72b16103ea7e3571a9e16b
Logan <mlogan858@hotmail.com>
2021-09-19 15:18:04 -0400

    updates for PR 8 feedback

--- server/controllers/requests.js
+++ server/controllers/requests.js
@@ -1,62 +1,61 @@
 const Request = require('../models/Request')
-module.exports = {
-    getReqs,
-    updateReqs, 
-    createReqs,
-}
+
 function getReqs(req, res, next) {
     try {
-        Request.find({sitter_id: req.body.id}, (err, post) => {
-            res.status(200).json(post)
+        if (!req.user.id) {
+            return res.sendStatus(401)
+        }
+        Request.find({ $or: [{ userId: req.user.id }, { sitterId: req.user.id }] 
+            }, (err, post) => {
+                res.status(200).json(post)
         })
     } catch (error) {
         next(error)
     }
 }
-function updateReqs(req, res, next) {
+
+async function updateReqs(req, res, next) {
     let updateDoc
     if (req.body.accepted) {
         updateDoc = {
             accepted: true
         }
-    } else if (req.body.declined) {
+    } else {
         updateDoc = {
             declined : true
         }
     }
     try {
-        Request.updateOneAndUpdate({_id: req.body.id}, {
+        await Request.findOneAndUpdate({_id: req.body.id}, {
             $set: updateDoc
-        }, 
-            (err, result) => {
-                if (err) {
-                    res.send(error)
-                } else {
-                    res.status(200).json(result)
-                }
-            }
-        )
+        })
+        res.status(200).json({ message: "Updated Successfully"})
     } catch (error) {
         next(error)
     }
 }
-function createReqs(req, res, next) {
+
+async function createReqs(req, res, next) {
     try {
-        Request.create({
+        if (!req.body.userId || !req.body.sitterId || !req.body.start || !req.body.end) {
+            return res.sendStatus(400)
+        }
+        await Request.create({
             user_id: req.body.id,
             sitter_id: req.body.sitter_id,
             start: req.body.start,
             end: req.body.end,
             dogType: req.body.dogType,
             specialNotes: req.body.specialNotes,
-        }, (err, post) => {
-            if (err) {
-                res.send(error)
-            } else {
-                res.status(200).json(post)
-            }
         })
+        res.status(200).json({message: "Created Successfuly"})
     } catch (error) {
         next(error)
     }
+}
+
+module.exports = {
+    getReqs,
+    updateReqs, 
+    createReqs,
 }
