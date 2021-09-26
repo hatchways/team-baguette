2d58127bf5dcd9026622292fcd20b216f4008108
Luis Reyes <luisreyesxv@yahoo.com>
2021-09-26 08:30:07 -0400

    cleaned up profile controller a bit to deal with getting the user object based on the protect middleware vs manually searching for it in the controller

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -15,11 +15,7 @@ exports.createProfile = asyncHandler(async (req, res, next) => {
     address,
     description,
   } = req.body;
-  const user = await User.findById(req.user.id);
-  if (!user) {
-    res.status(401);
-    throw new Error("Not authorized");
-  }
+  const user = req.user
   const profile = await Profile.findOne({ userId: user._id });
   if (profile) {
     res.status(400);
@@ -59,14 +55,9 @@ exports.updateProfile = asyncHandler(async (req, res, next) => {
     address,
     description,
   } = req.body;
-  const id = req.user.id;
-  const user = await User.findOne({ _id: id });
-  if (!user) {
-    res.status(401);
-    throw new Error("Not authorized");
-  }
+  const user = req.user;
   const profile = await Profile.findOneAndUpdate(
-    { userId: id },
+    { userId: user._id },
     {
       firstName: firstName,
       lastName: lastName,
@@ -92,17 +83,13 @@ exports.updateProfile = asyncHandler(async (req, res, next) => {
 // @desc Get profile by id
 // @access Public
 exports.getProfileById = asyncHandler(async (req, res, next) => {
-  const id = req.params.id;
-  const profile = await Profile.findOne({ userId: id });
+  const user = req.user
+  const profile = await Profile.findOne({ userId: user._id });
   if (!profile) {
     res.status(404);
     throw new Error("No profile");
   }
-  const user = await User.findOne({ _id: id });
-  if (!user) {
-    res.status(404);
-    throw new Error("No User");
-  }
+
   const convertedJSON = profile.toJSON();
   convertedJSON.email = user.email;
   res.status(200).json({
