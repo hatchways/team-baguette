68b2f36bc16d4e6a08a0d4794844a3fcc125fd4a
Logan <mlogan858@hotmail.com>
2021-10-06 15:46:17 -0400

    removed switch

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { Route, Redirect, RouteProps, Switch } from 'react-router-dom';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { useAuth } from '../../context/useAuthContext';
 import { CircularProgress } from '@material-ui/core';
 
@@ -14,12 +14,10 @@ const ProtectedRoute = ({ component: Component, ...rest }: IProps): JSX.Element 
   if (isLoading) return <CircularProgress />;
 
   return (
-    <Switch>
-      <Route
-        {...rest}
-        render={(routeProps) => (loggedInUser ? <Component {...routeProps} /> : <Redirect to={'/login'} />)}
-      />
-    </Switch>
+    <Route
+      {...rest}
+      render={(routeProps) => (loggedInUser ? <Component {...routeProps} /> : <Redirect to={'/login'} />)}
+    />
   );
 };
 
